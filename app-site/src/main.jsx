a908edf5c9ec5aa9e49d5b1c1d2f9fa2e1b56f1c
jrvinyard-design <jrvinyard@gmail.com>
2026-07-13 08:19:12 -0500

    Add files via upload

---
+++ app-site/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RTBoardPrep from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RTBoardPrep />
+  </React.StrictMode>,
+)
